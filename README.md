05f4383f9b87cd7fa99f5f499736eec26bd96dc4
Mikey Binns <38146638+mikeybinns@users.noreply.github.com>
2025-04-11 17:05:33 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# compiler-helpers
-A group of PHP helpers for the new AS compiler npm package.
+# AS compiler helpers
+
+A group of PHP helpers for the new AS compiler. For use with the npm package `@atomicsmash/compiler`.
+
+## To release a new version:
+
+1. Make any changes you need to the package
+2. Merge into main
+3. Create a new tag with the new version number, which will automatically push to packagist.
+   This could be automated later with auto tag creation on merge to main, but currently it's a manual process.
---
+++ classes/Assets.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Assets class
+ */
+
+namespace AtomicSmash\CompilerHelpers;
+
+use Error;
+
+/**
+ * The base class for getting assets from the new compiler.
+ */
+abstract class Assets {
+	/**
+	 * Context folder e.g. the theme or plugin root folder. Prefer a fixed folder path over get_stylesheet_directory().
+	 *
+	 * @var string|null
+	 */
+	protected $context_folder = null;
+
+	/**
+	 * The folder to find the built assets.
+	 *
+	 * @var string
+	 */
+	private $build_folder = '/dist/';
+
+	/**
+	 * Manifest content
+	 *
+	 * @var array
+	 */
+	public $manifest;
+
+	/**
+	 * WP asset info file.
+	 *
+	 * @var array
+	 */
+	public $asset_info;
+
+	/**
+	 * Constructor
+	 *
+	 * @throws Error If trying to use the class directly.
+	 */
+	public function __construct() {
+		if ( null === $this->context_folder ) {
+			throw new Error( 'Don\'t use the assets class directly. Instead, extend it and add the context folder parameter.' );
+		}
+		if ( ! str_ends_with( $this->build_folder, '/' ) ) {
+			$this->build_folder = $this->build_folder . '/';
+		}
+		if ( ! str_ends_with( $this->context_folder, '/' ) ) {
+			if ( str_starts_with( $this->build_folder, '/' ) ) {
+				$this->build_folder = substr( $this->build_folder, 1 );
+			}
+			$this->context_folder = $this->context_folder . '/';
+		}
+		$this->manifest = $this->get_manifest();
+		$this->asset_info = $this->get_asset_info();
+	}
+
+	/**
+	 * Get the asset manifest
+	 */
+	private function get_manifest() {
+		$manifest_locations = array(
+			'assets-manifest.json',
+			'mix-manifest.json', // Legacy
+		);
+		foreach ( $manifest_locations as $manifest_location ) {
+			if ( file_exists( $this->context_folder . $this->build_folder . $manifest_location ) ) {
+				return wp_json_file_decode(
+					$this->context_folder . $this->build_folder . $manifest_location,
+					array(
+						'associative' => true,
+					)
+				);
+			}
+		}
+		return array();
+	}
+
+	/**
+	 * Get the WordPress asset info
+	 */
+	private function get_asset_info() {
+		$asset_info_locations = array(
+			'wordpress-assets-info.php',
+			'assets.php', // Legacy
+		);
+		foreach ( $asset_info_locations as $asset_info_location ) {
+			if ( file_exists( $this->context_folder . $this->build_folder . $asset_info_location ) ) {
+				return require $this->context_folder . $this->build_folder . $asset_info_location;
+			}
+		}
+		return array();
+	}
+
+	/**
+	 * Get cached asset
+	 *
+	 * Gets the file info of a cached asset from the manifest file. This function will search for an entry in the child theme manifest first, falling back to the manifest in the parent theme, and returning an error if it's not found in either.
+	 *
+	 * @param string $filename Name of the file in the manifest.
+	 * @return array A collection of info on the asset, including source, dependencies and version, or null if the asset can't be found.
+	 */
+	public function get_cached_asset( string $filename ) {
+		$asset = $this->manifest[ $filename ] ?? null;
+		if ( null === $asset ) {
+			return null;
+		}
+		return array(
+			'source' => $this->context_folder . $this->build_folder . $asset,
+			'dependencies' => isset( $this->asset_info[ $filename ] ) ? $this->asset_info[ $filename ]['dependencies'] : array(),
+			'version' => isset( $this->asset_info[ $filename ] ) ? $this->asset_info[ $filename ]['version'] : null,
+		);
+	}
+}
